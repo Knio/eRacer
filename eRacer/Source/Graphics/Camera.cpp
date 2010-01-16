da14e4d1f014216b9bd3f0420717b58375f3dfa7
olrehm <ole@rehmsen.de>
2010-01-16 14:19:06 -0700

    doxygen for Camera, fixed bug where I passed Vector instead of pointer

--- eRacer/Source/Graphics/Camera.cpp
+++ eRacer/Source/Graphics/Camera.cpp
@@ -10,7 +10,11 @@
 
 Camera::Camera(const Point3& position, const Point3& lookAt, const Vector3& approxUp)
 	: position_(position),
-	  lookAt_(lookAt)
+	  lookAt_(lookAt),
+	  near_(1.0f),
+	  far_(100.0f),
+	  aspectRatio_(0.75f),
+	  fovY_(PI*0.25f)
 {
 	setUp(approxUp);
 }
@@ -33,11 +37,13 @@ void Camera::updatePlanes(){
 
 	//near
 	planes_[4].normal = lookAt_-position_;
-	D3DXVec3Normalize(planes_[4].normal,planes_[4].normal);
-	planes_[4].distance = D3DXVec3Dot(planes_[4].normal, position_+near_*planes_[4].normal);
+	D3DXVec3Normalize(&planes_[4].normal,&planes_[4].normal);
+	Point3 onPlane = position_+near_*planes_[4].normal;
+	planes_[4].distance = D3DXVec3Dot(&planes_[4].normal, &onPlane);
 
 	//far
 	planes_[5].normal = -planes_[4].normal;
-	planes_[5].distance = D3DXVec3Dot(planes_[5].normal, position_+far_*planes_[4].normal);
+	onPlane = position_+far_*planes_[4].normal;
+	planes_[5].distance = D3DXVec3Dot(&planes_[5].normal, &onPlane);
 }
 
