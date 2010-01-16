6065a458edfcf989694f043bdf76ca0010fe850d
olrehm <ole@rehmsen.de>
2010-01-15 23:40:51 -0700

    started computing the planes - first need to do vector wrapper

--- eRacer/Source/Graphics/Camera.cpp
+++ eRacer/Source/Graphics/Camera.cpp
@@ -17,3 +17,27 @@ Camera::Camera(const Point3& position, const Point3& lookAt, const Vector3& appr
 
 Camera::~Camera() {
 }
+
+void Camera::updatePlanes(){
+	//TODO
+
+	//left
+
+
+
+	//right
+
+	//bottom
+
+	//top
+
+	//near
+	planes_[4].normal = lookAt_-position_;
+	D3DXVec3Normalize(planes_[4].normal,planes_[4].normal);
+	planes_[4].distance = D3DXVec3Dot(planes_[4].normal, position_+near_*planes_[4].normal);
+
+	//far
+	planes_[5].normal = -planes_[4].normal;
+	planes_[5].distance = D3DXVec3Dot(planes_[5].normal, position_+far_*planes_[4].normal);
+}
+
