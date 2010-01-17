32889e0bf766b8da8ded2b12c742328fab044dee
Michael Blackadar <Michael@.(none)>
2010-01-17 00:12:03 -0700

    Added PhysicsObject class, but doesn't do anything useful. Added more basic physics set up stuff.

--- eRacer/Source/Physics/PhysicsLayer.cpp
+++ eRacer/Source/Physics/PhysicsLayer.cpp
@@ -8,8 +8,10 @@
 
 #include "PhysicsLayer.h"
 
-PhysicsLayer::PhysicsLayer(){
+NxPhysicsSDK* PhysicsLayer::gPhySDK = NULL;
+NxScene* PhysicsLayer::gPhyScene = NULL;
 
+PhysicsLayer::PhysicsLayer(){
 }
 
 PhysicsLayer::~PhysicsLayer(){
@@ -17,19 +19,32 @@ PhysicsLayer::~PhysicsLayer(){
 }
 
 void PhysicsLayer::Init(){
-	gPhysicsSDK = NULL;
-	NxPhysicsSDKDesc desc;
-	gPhysicsSDK = NxCreatePhysicsSDK(NX_PHYSICS_SDK_VERSION, NULL, NULL, desc, NULL);
-	if(gPhysicsSDK == NULL) 
-	{
-		std::cout << "PHYSX failed to inialize" << std::endl;
+	gPhySDK = NULL;
+	gPhySDK = NxCreatePhysicsSDK(NX_PHYSICS_SDK_VERSION);
+	if(gPhySDK == NULL){
+		std::cout << "PHYSX failed to initialize" << std::endl;
+		return;
 	}
 	else{
-		std::cout << "PHYSX inialized" << std::endl;
+		std::cout << "PHYSX initialized" << std::endl;
 	}
+	NxSceneDesc sceneDesc;
+	sceneDesc.gravity = NxVec3(0, -9.8, 0);
+	gPhyScene = gPhySDK->createScene(sceneDesc);
+	sceneDesc.simType = NX_SIMULATION_SW;
+
+    // Create the default material
+    NxMaterial* defaultMaterial = gPhyScene->getMaterialFromIndex(0); 
+    defaultMaterial->setRestitution(0.5);
+    defaultMaterial->setStaticFriction(0.5);
+    defaultMaterial->setDynamicFriction(0.5);
 }
 
 void PhysicsLayer::Shutdown(){
-	gPhysicsSDK->release();
+	gPhySDK->release();
 	std::cout << "PHYSX released" << std::endl;
+}
+
+void PhysicsLayer::AddPhyObj(PhysicsObject& phyObj){
+	phyObjs.push_back(phyObj);
 }
