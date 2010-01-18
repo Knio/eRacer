1a8de344d99b92c86e871408a8ebbcb451349077
olrehm <ole@rehmsen.de>
2010-01-18 16:36:54 -0700

    created scene class

---
+++ eRacer/Source/Graphics/Scene.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file Scene.cpp
+ * @brief Implementation of the Scene class
+ *
+ * @date 18.01.2010
+ * @author: Ole Rehmsen
+ */
+
+#include "Scene.h"
+
+namespace Graphics {
+
+
+Scene::Scene()
+{
+	
+}
+
+Scene::~Scene(){
+}
+
+
+
+};
---
+++ eRacer/Source/Graphics/Scene.h
@@ -0,0 +1,77 @@
+/**
+ * @file Scene.h
+ * @brief Definition of the Scene class
+ *
+ * @date 18.01.2010
+ * @author: Ole Rehmsen
+ */
+
+#pragma once
+
+#include <vector>
+#include "Camera.h"
+#include "GeometryNode.h"
+#include "MovingGeometryNode.h"
+
+using namespace std;
+
+namespace Graphics {
+
+
+/**
+ * @brief Provides an abstraction for whatever internal organisation of the scene may be. 
+ */
+class Scene
+{
+public:
+	/**
+	 * @brief Constructor stub.
+	 */
+	Scene();
+
+	/**
+	 * @brief Destructor stub.
+	 */
+	virtual ~Scene();
+
+	/**
+	 * @brief Fill a vector with all geometry nodes in the scene visible with a given camera.
+	 *
+	 * This method will just append pointers to the vectors, not checking whether it was empty before.
+	 *
+	 * @param camera 
+	 *			a camera defining the view frustum against which the scene is culled
+	 * @param visibleNodes
+	 *			the list the visible geometry nodes are appended to 
+	 *
+	 */
+	void GetVisibleNodes(const Camera& camera, vector<GeometryNode*>& visibleNodes);
+
+	/**
+	 * @brief Factory method to create new non-moving geometry in the scene
+	 *
+	 * Static geometry does not change its transform or its mesh over time.
+	 * This is useful for optimization of the culling process.
+	 * 
+	 * @param transform
+	 *			a transformation matrix for the geometry
+	 * @return a pointer to the newly created geometry
+	 *
+	 * @see CreateGeometry
+	 */ 
+	GeometryNode* CreateStaticGeometry(const Matrix& transform);
+
+	/**
+	 * @brief Factory method to create new geometry in the scene
+	 *
+	 * @param transform
+	 *			a transformation matrix for the geometry
+	 * @return a pointer to the newly created geometry
+	 */ 
+	MovingGeometryNode* CreateMovingGeometry(const Matrix& transform);
+
+
+};
+
+
+};
