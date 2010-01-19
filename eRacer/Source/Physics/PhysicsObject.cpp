437b6990e0b7e7d6d60d10add4a03b2283b55509
unknown <John@.(none)>
2010-01-18 20:25:39 -0700

    Added funcitonality to the Physics layer and component

--- eRacer/Source/Physics/PhysicsObject.cpp
+++ eRacer/Source/Physics/PhysicsObject.cpp
@@ -1,40 +1,29 @@
 #include "PhysicsObject.h"
 
-PhysicsObject::PhysicsObject(){
-	actor = createBox();
+PhysicsObject::PhysicsObject()
+{
 }
 
-PhysicsObject::~PhysicsObject(){
-
+PhysicsObject::~PhysicsObject()
+{
 }
 
-//copied from nvidia sample docs
-NxActor* PhysicsObject::createBox(){
-	// Set the box starting height to 3.5m so box starts off falling onto the ground
-	NxReal boxStartHeight = 3.5; 
+void PhysicsObject::setMass(float f)
+{
+	Actor->setMass((NxReal)f);
+}
 
-	// Add a single-shape actor to the scene
-	NxActorDesc actorDesc;
-	NxBodyDesc bodyDesc;
+float PhysicsObject::getMass()
+{
+	return (float)Actor->getMass();
+}
 
-	// The actor has one shape, a box, 1m on a side
-	NxBoxShapeDesc boxDesc;
-	boxDesc.dimensions.set(0.5,0.5,0.5);
-	boxDesc.localPose.t = NxVec3(0, 0, 0);
-	actorDesc.shapes.pushBack(&boxDesc);
+NxMat33 PhysicsObject::returnOrientationMatrix()
+{
+	return Actor->getGlobalOrientation();
+}
 
-	actorDesc.body			= &bodyDesc;
-	actorDesc.density		= 10.0f;
-	actorDesc.globalPose.t	= NxVec3(0,boxStartHeight,0);	
-	assert(actorDesc.isValid());
-	NxActor *pActor = PhysicsLayer::gPhyScene->createActor(actorDesc);	
-	//assert(pActor);
-
-	// //create actor with no shapes
-	//NxShape* const *shape = pActor->getShapes();
-	//NxBoxShape *boxShape = shape[0]->isBox();
-	//assert(boxShape);
-	//pActor->releaseShape(*boxShape);
-
-	return pActor;
+NxActor* PhysicsObject::returnActor()
+{
+	return Actor;
 }
