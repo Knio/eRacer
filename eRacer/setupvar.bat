1bcea2c62022d8afc710b6a1135f5f158a76496f
Tom Flanagan <theknio@gmail.com>
2010-01-22 12:21:15 -0700

    fix swig

--- eRacer/setupvar.bat
+++ eRacer/setupvar.bat
@@ -1,5 +1,4 @@
-set SWIG_DIR=W:\swigwin-1.3.40
+set SWIG_DIR=C:\Program Files\swigwin-1.3.40\swigwin-1.3.40\
 set PYTHON_DIR=C:\Python26
 set PHYSX_DIR=C:\Program Files\NVIDIA Corporation\NVIDIA PhysX SDK\v2.8.1
-set BOOST_DIR=C:\Temp\boost_1_41_0\boost_1_41_0
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" eRacer.sln
