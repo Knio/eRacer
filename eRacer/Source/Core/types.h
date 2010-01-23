ce1b88391cc1b84f1a860fa945e32aeb219fc071
olrehm <ole@rehmsen.de>
2010-01-22 18:58:23 -0700

    some adjustments to how doxygen generates the docs documented Plane

--- eRacer/Source/Core/types.h
+++ eRacer/Source/Core/types.h
@@ -5,7 +5,11 @@
 #include <d3dx9math.h>
 #include "Math.h"
 
-
+/**
+ * @brief A simple datastructure to store a plane
+ *
+ * The plane consists of all v such that dot(normal, v) = distance
+ */
 struct Plane {
 	Vector3 normal;
 	float distance;
