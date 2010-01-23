0877912eacb9457716c691d6119260847597a297
Tom Flanagan <theknio@gmail.com>
2010-01-22 22:45:06 -0700

    created build script

--- eRacer/run.py
+++ eRacer/run.py
@@ -2,9 +2,9 @@ import os
 import sys
 import time
 # paths to code
-sys.path.append('Release')
-sys.path.append('Source')
-sys.path.append('Source\\Swig')
+sys.path.append(os.path.join(sys.path[0], 'Release'))
+sys.path.append(os.path.join(sys.path[0], 'Source'))
+sys.path.append(os.path.join(sys.path[0], 'Source\\Swig'))
 
 # set path to load fmod.dll
 os.environ['PATH'] += ';Lib'
--- eRacer/setup.py
+++ eRacer/setup.py
@@ -1,14 +1,35 @@
 from distutils.core import setup
 import py2exe
+import glob
+import sys
+import os
  
+# paths to code
+sys.path.append(os.path.join(sys.path[0], 'Release'))
+sys.path.append(os.path.join(sys.path[0], 'Source'))
+sys.path.append(os.path.join(sys.path[0], 'Source\\Swig'))
+sys.path.append(os.path.join(sys.path[0], 'Lib'))
+
+print sys.path
+
 setup(
   name='eRacer',
   version='1.0',
-  packages=[
-    'Source', 
-    'Source/Graphics', 
-    'Release',
-    'Lib',
-  ],
-  console=['run.py']
+  console=['run.py'],
+  zipfile='eRacer.data',
+  options={
+    'py2exe': {
+      'includes':[
+        'ctypes',
+        'eRacer',
+      ],
+      'packages':[
+        'Source'
+      ],
+      'bundle_files': 3,
+      'compressed': True,
+      'dist_dir':'T:/dist'
+    }
+  },
+  data_files=[(i, glob.glob(i+'/*')) for i in glob.glob('Resources/*')]
 )
