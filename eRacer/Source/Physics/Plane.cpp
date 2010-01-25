c45dd72672f2e672b9940170e6cb93eef55079ad
olrehm <ole@rehmsen.de>
2010-01-24 23:11:45 -0700

    added some friction

--- eRacer/Source/Physics/Plane.cpp
+++ eRacer/Source/Physics/Plane.cpp
@@ -5,8 +5,16 @@ Plane::Plane(bool dynamic, float mass, float distOrigin, Vector3 normal){
 	PhysicsObject::dynamic = dynamic;
 	PhysicsObject::mass = (dynamic ? mass : 0);
 
+	NxMaterialDesc material;
+	material.restitution = 0.5;
+	material.dynamicFriction = 0.5;
+	material.staticFriction = 0.5;
+
     // Create a plane with default descriptor
     NxPlaneShapeDesc planeDesc;
+
+	planeDesc.materialIndex = PhysicsLayer::g_PhysicsLayer->AddMaterialReturnIndex(material);
+
 	planeDesc.d = distOrigin;
 	planeDesc.normal = NxVec3(normal.x, normal.y, normal.z);
     NxActorDesc actorDesc;
