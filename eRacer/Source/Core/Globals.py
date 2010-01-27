2b3a1a18770c86894ddecd2369166bd5dd46a217
Tom Flanagan <theknio@gmail.com>
2010-01-27 13:59:05 -0700

    begin cleaning up python imports

---
+++ eRacer/Source/Core/Globals.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import math
+
+import eRacer
+
+from Core 	import Event, Util
+from Game	import Module, Entity
+
+# global game object
+game = Util.Proxy(None)
+
+class Vector3(eRacer.D3DXVECTOR3): pass
+class Vector4(eRacer.D3DXVECTOR4): pass
+class Point3(eRacer.D3DXVECTOR3): pass
+class Point4(eRacer.D3DXVECTOR4): pass
+
+
+
+
+
+
+
