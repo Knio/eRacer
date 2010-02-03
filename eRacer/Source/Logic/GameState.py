c69076faad9990578f823ddf0e760897c25da346
Tom Flanagan <theknio@gmail.com>
2010-02-03 13:00:34 -0700

    font size const. menus!\

--- eRacer/Source/Logic/GameState.py
+++ eRacer/Source/Logic/GameState.py
@@ -41,7 +41,6 @@ class GameState(State):
     
   def Tick(self, time):
     State.Tick(self, time)
-    print time.game_delta
     game().graphics.scene  = self.scene
     game().graphics.camera = self.camera
     #game().graphics.views.append(self.view)
