3a695855c707a8351977bc970c2b3bcc93befced
Ole Rehmsen <ole@rehmsen.de>
2010-02-18 20:07:00 -0700

    documented Renderable

--- eRacer/Source/Graphics/Renderable.h
+++ eRacer/Source/Graphics/Renderable.h
@@ -11,11 +11,26 @@
 
 #include "D3D9.h"
 
+/**
+ * @brief Abstract base class for all renderable objects
+ *
+ * A renderable is able to draw itself to a Direct3D device.
+ */
 class Renderable
 {
 public:
+	/**
+	 * @brief Virtual destructor stub so sub class destructors are called
+	 */
 	virtual ~Renderable() {}
-	virtual void Draw(IDirect3DDevice9*) const = 0;
+
+	/**
+	 * @brief Draw this renderable. To be implemented by sub classes.
+	 *
+	 * @param device
+	 *			the device to draw to
+	 */
+	virtual void Draw(IDirect3DDevice9* device) const = 0;
 };
 
 #endif
