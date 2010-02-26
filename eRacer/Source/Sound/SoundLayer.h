9c5e97917e3bcee0b827c64eb2f922752b2e7c52
don <had@ucalgary.ca>
2010-02-26 12:18:20 -0700

    Sound Integration, Part 1

--- eRacer/Source/Sound/SoundLayer.h
+++ eRacer/Source/Sound/SoundLayer.h
@@ -20,6 +20,7 @@
 #include "fmod.h"
 #include "fmod_errors.h"
 #include "Core\types.h"
+#include <vector>
 
 #undef min
 #undef max
@@ -30,11 +31,50 @@ namespace Sound {
 
 typedef map<string, FSOUND_SAMPLE*> FontCache;
 
+struct SoundFx 
+{
+public:
+	FSOUND_SAMPLE* soundsample;
+	int channel;
+	int pitch;
+	bool isPaused;
+	bool isLooping;
+
+	//3D positional info
+	bool is3D;
+	Point3 position;
+	Vector3 velocity;
+	float maxDist;
+	float minDist;
+
+   SoundFx() 
+   {
+      channel = -1;
+	  pitch = 44100;
+	  isPaused = false;
+	  is3D = false;
+	  isLooping = true;
+	  position = Point3(0,0,0);
+	  velocity = Vector3(0,0,0);
+	  maxDist = 10000.0f;
+	  minDist = 50.0f;
+   }
+   ~SoundFx()
+   {
+		if (NULL!= soundsample)
+			FSOUND_Sample_Free(soundsample); 
+
+   }
+
+};
+
+
 class SoundLayer
 {
 private:
 	static SoundLayer *m_pGlobalSoundLayer;
-	FMUSIC_MODULE *m_fmodule;
+
+	FSOUND_SAMPLE* m_musicSample;
 
 	//map<const char*, FMUSIC_MODULE*> m_MusicCache;
 	FontCache m_SoundCache2D;
@@ -48,74 +88,20 @@ protected:
 public:
 	~SoundLayer();	//Destructor
 
-	/**
-	 * @brief Call Init to initiliaze the sound layer.
-	 * @return Poorly defined error code
-	 */
 	int Init();
-
-	/**
-	 * @brief Update must be called each frame when using 3D sound to dynamically change the sounds
-	 * @return Poorly defined error code
-	 */
 	int Update();
-
-	/**
-	 * @brief For 3D Sound. Set the listener parameter and orientation
-	 * 
-	 * @param listenerPos Listener Position. Usually the camera position
-	 * @param listenerVel Listener Velocity for doppler effects. In meters per second
-	 * @param atVector The direction the listener is facing
-	 * @param upVector The up direction for the listener
-	 * @return Poorly defined error code
-	 */
 	int SetOrientation3D(const Point3& listenerPos, const Vector3& listenerVel, const Vector3& atVector, const Vector3& upVector); //For 3D sound
 
-	/**
-	 * @brief Play sound in 3D Space
-	 * 
-	 * @param name Resource file name
-	 * @param pos Position to play the sound
-	 * @param vel Velocity vector for doppler effects. In meters per second
-	 * @return The channel the sound is playing on
-	 */
-	int PlaySound3D(const string& name, const Point3& pos, const Vector3& vel);
+	void LoadSoundFx(const string& filename, SoundFx* samp);
+	void UpdateSoundFx(SoundFx* samp);
 
-	/**
-	 * @brief Play sound in 2D Space
-	 * 
-	 * @param name Resource file name
-	 * @return The channel the sound is playing on
-	 */
-	int PlaySound2D(const string& name);
-
-	/**
-	 * @brief Changes the pitch for a sound being played on a given channel
-	 * 
-	 * @param channel The Channel the sound is playing on. Get the channel from the PlaySound return code.
-	 * @param pitch Playback rate for the sound. Normal is around 22000. Higher numbers mean higher pitch
-	 * @return Poorly defined error code
-	 */
-	int ChangePitch(int channel, int pitch);
-
-	/**
-	 * @brief Play looped music. Currently a limit of 1 song
-	 * 
-	 * @param name Resource file name
-	 * @return The channel the sound is playing on
-	 */
 	int LoopMusic(const string& name);
-
-	/**
-	 * @brief Stop looped music
-	 */
 	int StopMusic();
 
-	/**
-	 * @brief Call Shutdown to close sound objects and free memory
-	 *
-	 * @return Poorly defined error code
-	 */
+	int PlaySound3D(const string& name, const Point3& pos, const Vector3& vel);
+	int PlaySound2D(const string& name);
+	int ChangePitch(int channel, int pitch);
+
 	int Shutdown();
 
 	static SoundLayer *GetInstance()
