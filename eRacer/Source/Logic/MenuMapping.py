6339e59f17172713d9e8226aad5503cff2f0508d
Ole Rehmsen <ole@rehmsen.de>
2010-03-15 23:17:10 -0600

    menu gamepad timeout (needs testing) - fixes #167

--- eRacer/Source/Logic/MenuMapping.py
+++ eRacer/Source/Logic/MenuMapping.py
@@ -1,15 +1,27 @@
+import time
+
 from Core.Globals import *
 from Mapping      import Mapping, E
 
 class MenuMapping(Mapping):
+  MIN_INTERVAL = .5
+  
+  def __init__(self):
+    self.lastMenuTime = time.time()
+
+  
   def KeyPressedEvent(self, key):
     if key == KEY.UP:     return E.MenuUpEvent()
     if key == KEY.DOWN:   return E.MenuDownEvent()
     if key == KEY.RETURN: return E.MenuSelectEvent()
     
   def GamepadStick1AbsoluteEvent(self, x, y):
-    if y >  900.0:        return E.MenuUpEvent()
-    if y < -900.0:        return E.MenuDownEvent()
+    if y >  900.0 and (time.time()-self.lastMenuTime) > self.MIN_INTERVAL:
+      self.lastMenuTime = time.time()
+      return E.MenuUpEvent()
+    if y < -900.0 and (time.time()-self.lastMenuTime) > self.MIN_INTERVAL:
+      self.lastMenuTime = time.time()
+      return E.MenuDownEvent()
     
   def GamepadButtonPressedEvent(self, button):
     if button == cpp.BUTTON_A: return E.MenuSelectEvent()
