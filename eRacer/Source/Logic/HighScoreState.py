fd56572c6b38b244450d937f9d262c20377103e3
Ole Rehmsen <ole@rehmsen.de>
2010-04-18 22:42:17 -0600

    made high score state not crash if no file can be found

--- eRacer/Source/Logic/HighScoreState.py
+++ eRacer/Source/Logic/HighScoreState.py
@@ -1,9 +1,11 @@
-
 from Core.Globals     import *
 from Core.Menu        import *
 from Core.Config      import Config
 from MenuState        import MenuState
 from MenuMapping      import *
+import os
+import os.path
+
 
 class HighScoreState(MenuState):
   MAPPING = MainMenuMapping
@@ -14,30 +16,32 @@ class HighScoreState(MenuState):
     laps = {}
 
     self.stats = []
+    self.loaded = False
+    
+    self.menu = [
+      ApplyMenuItem('Back', self.Menu_Back),
+    ]
+
     try:
-      for line in open(Config.USER_STATS, 'r').readlines():
-        line = line.split('\t')
-        stat = Struct(track=line[0], laps=int(line[1]), name=line[2], time=float(line[3]))
-        self.stats.append(stat)
-        tracknames[stat.track] = 1
-        laps[stat.laps] = 1
+      if os.path.exists(Config.USER_STATS):
+        for line in open(Config.USER_STATS, 'r').readlines():
+          line = line.split('\t')
+          stat = Struct(track=line[0], laps=int(line[1]), name=line[2], time=float(line[3]))
+          self.stats.append(stat)
+          tracknames[stat.track] = 1
+          laps[stat.laps] = 1
+        self.current = []
+        self.curTrack = len(tracknames.keys())>0 and tracknames.keys()[0] or None
+        self.curLaps = len(laps.keys()) and laps.keys()[0] or None
+        self.update()    
+        self.menu.append(SelectMenuItem('Track', self.Menu_Track, [(i,) for i in tracknames], 0))
+        self.menu.append(SelectMenuItem('Laps', self.Menu_Laps, [(i and str(i) or 'Best',i) for i in laps.keys()], 0))
+        self.loaded = True
         
     except:
       import traceback
       traceback.print_exc()
-      
-    
-    self.current = []
-    self.curTrack = tracknames.keys()[0]
-    self.curLaps = laps.keys()[0]
-    self.update()    
-        
-    self.menu = [
-      ApplyMenuItem('Back', self.Menu_Back),
-      SelectMenuItem('Track', self.Menu_Track, [(i,) for i in tracknames], 0),
-      SelectMenuItem('Laps', self.Menu_Laps, [(i and str(i) or 'Best',i) for i in laps.keys()], 0),
-    ]
-    
+                  
     
   def update(self):
     self.current = [i for i in self.stats if i.track==self.curTrack and i.laps==self.curLaps]
@@ -45,20 +49,22 @@ class HighScoreState(MenuState):
     
   def Tick(self, time):
     MenuState.Tick(self, time)
-  
     
     y = self.menuTop + 200
     
-    header = self.curLaps and 'Total Time' or 'Best Lap'
-    self.view.WriteString('Player', 'Sony Sketch EF', 28, 100, y)
-    self.view.WriteString(header,   'Sony Sketch EF', 28, 500, y)
-    y += 40
-    
-    for i, stat in enumerate(self.current[:20]):
-      self.view.WriteString(str(i+1),           'Sony Sketch EF', 22, 60, y)
-      self.view.WriteString(stat.name,          'Sony Sketch EF', 22, 100, y)
-      self.view.WriteString('%.2f' % stat.time, 'Sony Sketch EF', 22, 500, y)
-      y += 30
+    if self.loaded:
+      header = self.curLaps and 'Total Time' or 'Best Lap'
+      self.view.WriteString('Player', 'Sony Sketch EF', 28, 100, y)
+      self.view.WriteString(header,   'Sony Sketch EF', 28, 500, y)
+      y += 40
+      
+      for i, stat in enumerate(self.current[:20]):
+        self.view.WriteString(str(i+1),           'Sony Sketch EF', 22, 60, y)
+        self.view.WriteString(stat.name,          'Sony Sketch EF', 22, 100, y)
+        self.view.WriteString('%.2f' % stat.time, 'Sony Sketch EF', 22, 500, y)
+        y += 30
+    else:
+      self.view.WriteString('Could not load high score file - have you played yet?', 'Sony Sketch EF', 28, 100, y)
     
     
   def Menu_Back(self):
