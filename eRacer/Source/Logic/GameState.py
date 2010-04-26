db1f246f955ee3a5f3c4f9e76dfbf47765a7f840
Ole Rehmsen <ole@rehmsen.de>
2010-04-25 20:10:10 -0600

    speed up by more than 10% by using 'StaticModel' for Rings

--- eRacer/Source/Logic/GameState.py
+++ eRacer/Source/Logic/GameState.py
@@ -127,7 +127,7 @@ class GameState(State):
     finishLineTransform = Matrix(40, 8, 1) * Matrix(startFrame.position+startFrame.up*0.1+startFrame.fw*-3, startFrame.fw, -startFrame.up)
     self.Add(Quad('FinishLine','FinishLine2.png',finishLineTransform))
     finishLineTransform = Matrix(6.35, 4.0, 4.0) * Matrix(startFrame.position+startFrame.up*0.1+startFrame.fw*-3, startFrame.up, startFrame.fw)
-    self.Add(Model('FinishLine','FinishLine.x',None,finishLineTransform))
+    self.Add(StaticModel('FinishLine','FinishLine.x',finishLineTransform))
 
     for x in [200., 2200.]:
       for i in xrange(64):
